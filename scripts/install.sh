d0432e4a2c0ad4530e37e4cc309a8f6362a7e050
Sliferghost <jaapoudejans@gmail.com>
2024-02-22 16:44:46 +0100

    Create a small install script for all dependencies

---
+++ scripts/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install Brew
+if ! command -v brew &> /dev/null
+then
+  echo "Couldn't find brew, installing"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Brew already installed!"
+fi
+
+# Update brew
+brew update && brew upgrade
+
+# Install packages
+brew install bat eza fzf raycast neovim ripgrep sesh stow tmux tree-sitter zoxide -y
+
+# Clone dotfiles
+git clone git@github.com:Sliferghost/dotfiles.git ~/dotfiles
+
+# Stow the dotfiles
+stow ~/dotfiles
