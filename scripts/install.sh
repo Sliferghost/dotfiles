5d788190af258a09fd9958c82100a6a672c1dad7
Jaap Oudejans <joudejans@bol.com>
2024-02-22 17:00:26 +0100

    Remove the git checkout and remove invalid -y flag

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,10 +13,7 @@ fi
 brew update && brew upgrade
 
 # Install packages
-brew install bat eza fzf raycast neovim ripgrep sesh stow tmux tree-sitter zoxide -y
-
-# Clone dotfiles
-git clone git@github.com:Sliferghost/dotfiles.git ~/dotfiles
+brew install bat eza fzf raycast neovim ripgrep sesh stow tmux tree-sitter zoxide
 
 # Stow the dotfiles
 stow ~/dotfiles
