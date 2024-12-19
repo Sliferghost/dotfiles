68a60fcd3c56a02442996923e3a1d017bb9e587d
Sliferghost <jaapoudejans@gmail.com>
2024-12-19 14:30:57 +0100

    Remove terminal plugin and formatting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -887,7 +887,7 @@ require('lazy').setup({
 
   require 'custom.plugins.autopairs',
   require 'custom.plugins.lint',
-  require 'custom.plugins.terminal',
+  -- require 'custom.plugins.terminal',
 
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
