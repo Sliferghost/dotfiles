5de9ae85680d5254df2d3e315939f73cf1f2ff76
Jaap Oudejans <joudejans@bol.com>
2025-01-10 15:04:44 +0100

    Updated dependencies and removed go language servers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -536,7 +536,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        gopls = {},
+        -- gopls = {},
 
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -576,8 +576,8 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Lua formatter
-        'gopls', -- Go language server
-        'golangci-lint', -- Go linter
+        -- 'gopls', -- Go language server
+        -- 'golangci-lint', -- Go linter
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
