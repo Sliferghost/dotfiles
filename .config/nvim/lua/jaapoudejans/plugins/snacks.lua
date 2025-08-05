f4298a24f82c925b22146ff14af9e464beeccbf3
Sliferghost <jaapoudejans@gmail.com>
2025-08-05 20:26:17 +0200

    Simplify snacks keymaps

--- .config/nvim/lua/jaapoudejans/plugins/snacks.lua
+++ .config/nvim/lua/jaapoudejans/plugins/snacks.lua
@@ -140,22 +140,32 @@ return {
             }
         },
         keys = {
-            { "<leader>lg", function() require("snacks").lazygit() end, desc = "Lazygit" },
-            { "<leader>gl", function() require("snacks").lazygit.log() end, desc = "Lazygit Logs" },
-            { "<leader>es", function() require("snacks").explorer() end, desc = "Open Snacks Explorer" },
-            { "<leader>rF", function() require("snacks").rename.rename_file() end, desc = "Fast Rename Current File" },
-            { "<leader>bx", function() require("snacks").bufdelete() end, desc = "Delete or Close Buffer (Confirm)"},
+            { "<leader>lg", function() Snacks.lazygit() end, desc = "Lazygit" },
+            { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Logs" },
+            { "<leader>es", function() Snacks.explorer() end, desc = "Open Snacks Explorer" },
+            { "<leader>rF", function() Snacks.rename.rename_file() end, desc = "Fast Rename Current File" },
+            { "<leader>bx", function() Snacks.bufdelete() end, desc = "Delete or Close Buffer (Confirm)"},
 
             -- Snacks Picker
-            { "<leader>pf", function() require("snacks").picker.files() end, desc = "Find Files"},
-            { "<leader>pc", function() require("snacks").picker.files({cwd = vim.fn.stdpath("config")}) end, desc = "Find Config File"},
-            { "<leader>ps", function() require("snacks").picker.grep() end, desc = "Grep Word"},
-            {"<leader>pws", function() require("snacks").picker.grep_word() end, desc = "Search Visual selection or Word", mode = {"n", "x"}},
-            {"<leader>pk", function() require("snacks").picker.keymaps({layout = "ivy"}) end, desc = "Search Keymaps"},
+            { "<leader>pf", function() Snacks.picker.files() end, desc = "Find Files"},
+            { "<leader>pc", function() Snacks.picker.files({cwd = vim.fn.stdpath("config")}) end, desc = "Find Config File"},
+            { "<leader>ps", function() Snacks.picker.grep() end, desc = "Grep Word"},
+            {"<leader>pws", function() Snacks.picker.grep_word() end, desc = "Search Visual selection or Word", mode = {"n", "x"}},
+            {"<leader>pk", function() Snacks.picker.keymaps({layout = "ivy"}) end, desc = "Search Keymaps"},
 
-            {"<leader>gbr", function() require("snacks").picker.git_branches({layout = "select"}) end, desc = "Switch Git Branches"},
-            {"<leader>th", function() require("snacks").picker.colorschemes({layout = "ivy"}) end, desc = "Switch Color Schemes"},
-            {"<leader>ph", function() require("snacks").picker.help() end, desc = "Help Pages"},
+            {"<leader>gbr", function() Snacks.picker.git_branches({layout = "select"}) end, desc = "Switch Git Branches"},
+            {"<leader>th", function() Snacks.picker.colorschemes({layout = "ivy"}) end, desc = "Switch Color Schemes"},
+            {"<leader>ph", function() Snacks.picker.help() end, desc = "Help Pages"},
         }
+    },
+    {
+        "folke/todo-comments.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        optional = true,
+        keys = {
+            { "<leader>pt", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+            { "<leader>pT", function() Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
+        },
     }
 }
+
