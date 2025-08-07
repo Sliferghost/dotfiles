eb9d99f08ea580e6fdc09aa95fcd000fad8264e5
Sliferghost <jaapoudejans@gmail.com>
2025-08-07 10:44:18 +0200

    Setup trouble and image support

--- .config/nvim/lua/jaapoudejans/core/autocmds.lua
+++ .config/nvim/lua/jaapoudejans/core/autocmds.lua
@@ -35,13 +35,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		map("<leader>cf", vim.lsp.buf.format, "Format")
 		map("<leader>gd", "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>", "Goto Definition in Vertical Split")
 		vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { desc = "LSP: Signature Documentation" })
-		vim.keymap.set(
-			"n",
-			"<leader>dd",
-			":Telescope diagnostics bufnr=0<CR>",
-			{ desc = "LSP: Show all diagnostics in current file" }
-		)
-		vim.keymap.set("n", "<leader>dD", ":Telescope diagnostics<CR>", { desc = "LSP: Show all diagnostics" })
 
 		local function client_supports_method(client, method, bufnr)
 			if vim.fn.has("nvim-0.11") == 1 then
--- .config/nvim/lua/jaapoudejans/plugins/snacks.lua
+++ .config/nvim/lua/jaapoudejans/plugins/snacks.lua
@@ -90,23 +90,34 @@ return {
 					},
 				},
 			},
-			-- image = {
-			--     enabled = true,
-			--     doc = {
-			--         float = true, -- show image on cursor hover
-			--         inline = false, -- show image inline
-			--         max_width = 50,
-			--         max_height = 30,
-			--         wo = {
-			--             wrap = false,
-			--         },
-			--     },
-			--     convert = {
-			--         notify = true,
-			--         command = "magick"
-			--     },
-			--     img_dirs = { "img", "images", "assets", "static", "public", "media", "attachments","Archives/All-Vault-Images/", "~/Library", "~/Downloads" },
-			-- },
+			image = {
+				enabled = true,
+				doc = {
+					float = true, -- show image on cursor hover
+					inline = false, -- show image inline
+					max_width = 50,
+					max_height = 30,
+					wo = {
+						wrap = false,
+					},
+				},
+				convert = {
+					notify = true,
+					command = "magick",
+				},
+				img_dirs = {
+					"img",
+					"images",
+					"assets",
+					"static",
+					"public",
+					"media",
+					"attachments",
+					"~/Pictures",
+					"~/Library",
+					"~/Downloads",
+				},
+			},
 		},
 		keys = {
 			{
