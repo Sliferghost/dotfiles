5d6aae5894a747d7926315bb25169012208af253
Sliferghost <jaapoudejans@gmail.com>
2025-08-13 10:22:04 +0200

    Add keymap for closing quicklists

--- .config/nvim/lua/jaapoudejans/core/keymaps.lua
+++ .config/nvim/lua/jaapoudejans/core/keymaps.lua
@@ -28,3 +28,5 @@ vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+
+vim.keymap.set("n", "<leader>qc", ":cclose<CR>", { desc = "Close quicklist" })
