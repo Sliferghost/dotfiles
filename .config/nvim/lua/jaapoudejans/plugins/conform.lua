3b7b7f8f87eebe15d452156d92d56b382d21944c
Sliferghost <jaapoudejans@gmail.com>
2025-08-26 16:20:32 +0200

    Do formatting for web technologies with biome

--- .config/nvim/lua/jaapoudejans/plugins/conform.lua
+++ .config/nvim/lua/jaapoudejans/plugins/conform.lua
@@ -5,11 +5,20 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
+				-- Backend development
 				go = {
 					"goimports",
 					"golines",
 					"gofumpt",
 				},
+				-- Web development
+				javascript = { "biome-check" },
+				typescript = { "biome-check" },
+				javascriptreact = { "biome-check" },
+				typescriptreact = { "biome-check" },
+				css = { "biome-check" },
+				html = { "biome-check" },
+				json = { "biome-check" },
 			},
 			format_on_save = {
 				timeout_ms = 500,
