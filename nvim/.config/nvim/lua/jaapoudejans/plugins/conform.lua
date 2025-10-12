1523a406177f60578d929f44384f9e66c57a8c31
Jaap Oudejans <jaapoudejans@gmail.com>
2025-10-12 20:50:50 +0200

    Added formatting with ktlint

--- nvim/.config/nvim/lua/jaapoudejans/plugins/conform.lua
+++ nvim/.config/nvim/lua/jaapoudejans/plugins/conform.lua
@@ -5,6 +5,8 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
+				-- Backend development
+				kotlin = { "ktlint" },
 				-- Web development
 				javascript = { "biome-check" },
 				typescript = { "biome-check" },
@@ -15,7 +17,7 @@ return {
 				json = { "biome-check" },
 			},
 			format_on_save = {
-				timeout_ms = 500,
+				timeout_ms = 1000,
 				lsp_format = "fallback",
 			},
 		})
